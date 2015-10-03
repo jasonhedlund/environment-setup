ab5d30d3ee261e40605ba5aff16243a5095d9cb9
Jason Hedlund <jhedlund@hawk.iit.edu>
2015-10-03 18:41:48 -0500

    Added file to configure environment

---
+++ install-env.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt-get update -y
+sudo apt-get install -y apache2
+
+echo "Hello!" > /tmp/hello.tex#!/bin/bash
+
+sudo apt-get update -y
+sudo apt-get install -y apache2
+sudo apt-get install -y git
+
+echo "Hello!" > /tmp/hello.txt
