adc1d58d74c69443b75b0ab67967838210036b91
Jason Hedlund <jhedlund@hawk.iit.edu>
2015-10-03 18:57:28 -0500

    Added code to pull index.html from environment git hub repository

--- install-env.sh
+++ install-env.sh
@@ -6,7 +6,10 @@ sudo apt-get install -y apache2
 echo "Hello!" > /tmp/hello.tex#!/bin/bash
 
 sudo apt-get update -y
-sudo apt-get install -y apache2
-sudo apt-get install -y git
+sudo apt-get install -y apache2 git
+
+git clone https://github.com/jasonhedlund/application-setup.git
+
+mv ./application-setup /var/www/html
 
 echo "Hello!" > /tmp/hello.txt
