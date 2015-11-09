418cc16e2e965532d20f7e7703266a0b2acac8d0
Jason Hedlund <jhedlund@hawk.iit.edu>
2015-11-08 21:00:55 -0600

    Changed script around for mini project 1, included setup.php file, as well as installing php, sql, and curl

--- install-env.sh
+++ install-env.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 sudo apt-get update -y
-sudo apt-get install -y apache2
+sudo apt-get install -y apache2 git php5 php5-curl mysql-client curl
 
-echo "Hello!" > /tmp/hello.tex#!/bin/bash
-
-sudo apt-get update -y
-sudo apt-get install -y apache2 git
+sudo curl -sS https://getcomposer.org/installer | php
+sudo php composer.phar require aws/aws-sdk-php
 
 git clone https://github.com/jasonhedlund/application-setup.git
 
 mv ./application-setup/* /var/www/html
+mv vendor /var/www/html
+
+php -f /var/www/html/setup.php
+
+sudo chmod 600 /var/www/html/setup.php
 
 echo "Hello!" > /tmp/hello.txt
