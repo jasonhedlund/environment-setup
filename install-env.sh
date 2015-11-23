40af64b80c1dfe992218403012b1b48ab2256744
Jason Hedlund <jhedlund@hawk.iit.edu>
2015-11-22 20:22:13 -0600

    Changed syntax back

--- install-env.sh
+++ install-env.sh
@@ -5,16 +5,16 @@ sudo apt-get install -y apache2 git php5 php5-mysql php5-curl mysql-client curl
 
 sudo curl -sS https://getcomposer.org/installer | php &> /tmp/getcomposer.txt
 
-sudo php composer.phar require aws/aws-sdk-php &> /tmp/runcomposer.txt
-
 git clone https://github.com/jasonhedlund/application-setup.git
 
 sudo mv ./application-setup/* /var/www/html
 
 sudo mv vendor /var/www/html &> /tmp/movevendor.txt
 
-sudo php composer.phar require aws/aws-sdk-php
+sudo php composer.phar require aws/aws-sdk-php &> /tmp/runcomposer.txt
 
 sudo php /var/www/html/setup.php &> /tmp/database-setup.txt
+
+sudo chmod 600 /var/www/html/setup.php
 
 echo "Webserver environment has been installed!" > /tmp/hello.txt
